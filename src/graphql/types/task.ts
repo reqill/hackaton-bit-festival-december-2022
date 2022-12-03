723e7be68c0c921a9d4debe02fe3342ae952b489
Mac-Adam <80308649+Mac-Adam@users.noreply.github.com>
2022-12-03 12:51:49 +0100

    feat(api): automatic task fitting

--- src/graphql/types/task.ts
+++ src/graphql/types/task.ts
@@ -1,4 +1,13 @@
-import { enumType, extendType, inputObjectType, nonNull, objectType, stringArg } from 'nexus';
+import { Task as dbTask } from '@prisma/client';
+import {
+  enumType,
+  extendType,
+  inputObjectType,
+  intArg,
+  nonNull,
+  objectType,
+  stringArg,
+} from 'nexus';
 import { Group } from './group';
 import { User } from './user';
 const Importance = enumType({
@@ -14,6 +23,7 @@ export const Task = objectType({
     t.boolean('planned');
     t.nullable.string('startDate');
     t.nullable.string('endDate');
+    t.nullable.int('suspectedDuration');
     t.field('importance', { type: Importance });
     t.list.field('users', {
       type: User,
@@ -81,6 +91,7 @@ export const TaskMutation = extendType({
       args: {
         name: nonNull(stringArg()),
         importance: nonNull(ImportanceInput),
+        suspectedDuration: intArg(),
       },
       resolve(_root, args, ctx) {
         return ctx.prisma.task.create({
@@ -88,6 +99,7 @@ export const TaskMutation = extendType({
             name: args.name,
             planned: false,
             importance: args.importance.importance,
+            suspectedDuration: args.suspectedDuration,
             users: { connect: { id: ctx?.user?.id } },
           },
         });
@@ -132,6 +144,7 @@ export const GroupTaskMutation = extendType({
         name: nonNull(stringArg()),
         importance: nonNull(ImportanceInput),
         groupId: nonNull(stringArg()),
+        suspectedDuration: intArg(),
       },
       resolve(_root, args, ctx) {
         return ctx.prisma.task.create({
@@ -139,9 +152,266 @@ export const GroupTaskMutation = extendType({
             name: args.name,
             planned: false,
             importance: args.importance.importance,
+            suspectedDuration: args.suspectedDuration,
             group: { connect: { id: args.groupId } },
           },
         });
+      },
+    });
+  },
+});
+
+const formatDate = (date: Date) => {
+  return (
+    date.getFullYear().toString() +
+    '/' +
+    (date.getMonth() + 1).toLocaleString('en-US', {
+      minimumIntegerDigits: 2,
+      useGrouping: false,
+    }) +
+    '/' +
+    date.getDate().toLocaleString('en-US', {
+      minimumIntegerDigits: 2,
+      useGrouping: false,
+    }) +
+    ' ' +
+    date.getHours().toLocaleString('en-US', {
+      minimumIntegerDigits: 2,
+      useGrouping: false,
+    }) +
+    ':' +
+    date.getMinutes().toLocaleString('en-US', {
+      minimumIntegerDigits: 2,
+      useGrouping: false,
+    })
+  );
+};
+
+export const FindPersonalFitMutation = extendType({
+  type: 'Mutation',
+  definition(t) {
+    t.nonNull.field('findFit', {
+      type: 'Task',
+      args: {
+        taskId: nonNull(stringArg()),
+        startDate: nonNull(stringArg()),
+        endDate: nonNull(stringArg()),
+        minStartTime: intArg(),
+        maxEndTime: intArg(),
+        calculationInterval: intArg(),
+      },
+      resolve(_root, args, ctx) {
+        const calcInterv = args.calculationInterval || 30;
+        let startDate = new Date(args.startDate);
+        let endDate = new Date(args.endDate);
+        const difference = endDate.getTime() - startDate.getTime();
+        const intervalCount = difference / (1000 * 60 * calcInterv);
+
+        const intervals: { CRITICAL: boolean; HIGH: boolean; MEDIUM: boolean; LOW: boolean }[] = [];
+        for (let i = 0; i < intervalCount; i++) {
+          intervals.push({ CRITICAL: false, HIGH: false, MEDIUM: false, LOW: false });
+        }
+
+        const getDateString = (idx: number) => {
+          const date = new Date(startDate.getTime() + idx * calcInterv * 60 * 1000);
+          return date.toString();
+        };
+
+        const validateDate = (date: Date) => {
+          let ok: boolean = true;
+          let intervalMinutes = date.getHours() * 60 + date.getMinutes();
+          if (args.minStartTime) {
+            if (intervalMinutes < args.minStartTime) {
+              ok = false;
+            }
+          }
+          if (args.maxEndTime) {
+            if (intervalMinutes > args.maxEndTime) {
+              ok = false;
+            }
+          }
+          return ok;
+        };
+        const getInterval = (date: string) => {
+          const intervalDate = new Date(date);
+          let interval: number =
+            (intervalDate.getTime() - startDate.getTime()) / (1000 * 60 * calcInterv);
+          if (interval < 0 || interval > intervalCount) {
+            return undefined;
+          }
+          if (validateDate(intervalDate)) {
+            return interval;
+          }
+          return undefined;
+        };
+        ctx.prisma.user
+          .findUnique({
+            where: { id: ctx.user?.id },
+            include: { tasks: true, groups: { include: { tasks: true } } },
+          })
+          .then((user: any) => {
+            const tasks = user.tasks;
+            user.groups.forEach((g: any) => {
+              tasks.push(...g.tasks);
+            });
+            if (tasks) {
+              tasks.forEach((task: dbTask) => {
+                if (task.startDate && task.endDate) {
+                  let s = getInterval(task.startDate);
+                  let e = getInterval(task.endDate);
+                  if (s && e) {
+                    for (let i = s; i <= e; i++) {
+                      intervals[i][task.importance] = true;
+                    }
+                  }
+                }
+              });
+
+              const taskLength =
+                tasks.filter((t: any) => t.id == args.taskId)[0].suspectedDuration / calcInterv;
+              for (let i = 0; i < intervalCount - taskLength; i++) {
+                let succes: boolean = true;
+                for (let x = 0; x < taskLength; x++) {
+                  const intDate = getDateString(i + x);
+                  if (
+                    validateDate(new Date(intDate)) &&
+                    !(
+                      intervals[i + x].CRITICAL ||
+                      intervals[i + x].HIGH ||
+                      intervals[i + x].LOW ||
+                      intervals[i + x].MEDIUM
+                    )
+                  ) {
+                  } else succes = false;
+                }
+                if (succes) {
+                  return ctx.prisma.task.update({
+                    where: { id: args.taskId },
+                    data: {
+                      planned: true,
+                      startDate: formatDate(new Date(getDateString(i))),
+                      endDate: formatDate(new Date(getDateString(i + taskLength))),
+                    },
+                  });
+                }
+              }
+            }
+          });
+      },
+    });
+  },
+});
+
+export const FindGroupFitMutation = extendType({
+  type: 'Mutation',
+  definition(t) {
+    t.nonNull.field('findGroupFit', {
+      type: 'Task',
+      args: {
+        taskId: nonNull(stringArg()),
+        startDate: nonNull(stringArg()),
+        endDate: nonNull(stringArg()),
+        groupId: nonNull(stringArg()),
+        minStartTime: intArg(),
+        maxEndTime: intArg(),
+        calculationInterval: intArg(),
+      },
+      resolve(_root, args, ctx) {
+        const calcInterv = args.calculationInterval || 30;
+        let startDate = new Date(args.startDate);
+        let endDate = new Date(args.endDate);
+        const difference = endDate.getTime() - startDate.getTime();
+        const intervalCount = difference / (1000 * 60 * calcInterv);
+
+        const intervals: { CRITICAL: boolean; HIGH: boolean; MEDIUM: boolean; LOW: boolean }[] = [];
+        for (let i = 0; i < intervalCount; i++) {
+          intervals.push({ CRITICAL: false, HIGH: false, MEDIUM: false, LOW: false });
+        }
+
+        const getDateString = (idx: number) => {
+          const date = new Date(startDate.getTime() + idx * calcInterv * 60 * 1000);
+          return date.toString();
+        };
+
+        const validateDate = (date: Date) => {
+          let ok: boolean = true;
+          let intervalMinutes = date.getHours() * 60 + date.getMinutes();
+          if (args.minStartTime) {
+            if (intervalMinutes < args.minStartTime) {
+              ok = false;
+            }
+          }
+          if (args.maxEndTime) {
+            if (intervalMinutes > args.maxEndTime) {
+              ok = false;
+            }
+          }
+          return ok;
+        };
+        const getInterval = (date: string) => {
+          const intervalDate = new Date(date);
+          let interval: number =
+            (intervalDate.getTime() - startDate.getTime()) / (1000 * 60 * calcInterv);
+          if (interval < 0 || interval > intervalCount) {
+            return undefined;
+          }
+          if (validateDate(intervalDate)) {
+            return interval;
+          }
+          return undefined;
+        };
+        ctx.prisma.group
+          .findUnique({
+            where: { id: args.groupId },
+            include: { tasks: true, users: { include: { tasks: true } } },
+          })
+          .then((group: any) => {
+            const tasks = group.tasks;
+            group.users.forEach((u: any) => {
+              tasks.push(...u.tasks);
+            });
+            if (tasks) {
+              tasks.forEach((task: dbTask) => {
+                if (task.startDate && task.endDate) {
+                  let s = getInterval(task.startDate);
+                  let e = getInterval(task.endDate);
+                  if (s && e) {
+                    for (let i = s; i <= e; i++) {
+                      intervals[i][task.importance] = true;
+                    }
+                  }
+                }
+              });
+              const taskLength =
+                tasks.filter((t: any) => t.id == args.taskId)[0].suspectedDuration / calcInterv;
+              for (let i = 0; i < intervalCount - taskLength; i++) {
+                let succes: boolean = true;
+                for (let x = 0; x < taskLength; x++) {
+                  const intDate = getDateString(i + x);
+                  if (
+                    validateDate(new Date(intDate)) &&
+                    !(
+                      intervals[i + x].CRITICAL ||
+                      intervals[i + x].HIGH ||
+                      intervals[i + x].LOW ||
+                      intervals[i + x].MEDIUM
+                    )
+                  ) {
+                  } else succes = false;
+                }
+                if (succes) {
+                  return ctx.prisma.task.update({
+                    where: { id: args.taskId },
+                    data: {
+                      planned: true,
+                      startDate: formatDate(new Date(getDateString(i))),
+                      endDate: formatDate(new Date(getDateString(i + taskLength))),
+                    },
+                  });
+                }
+              }
+            }
+          });
       },
     });
   },
