78d0f0881d06679380d1ecfaf471aba9a644387e
Mac-Adam <80308649+Mac-Adam@users.noreply.github.com>
2022-12-03 05:37:58 +0100

    feat(integration): added functions to retrieve and send data to api

--- pages/dashboard/board/index.tsx
+++ pages/dashboard/board/index.tsx
@@ -1,7 +1,49 @@
 import { Center, Heading } from '@chakra-ui/react';
+import { gql, useMutation, useQuery } from '@apollo/client';
 import Head from 'next/head';
 
+const GET_TASKS = gql`
+  query Query {
+    me {
+      tasks {
+        endDate
+        id
+        importance
+        name
+        planned
+        startDate
+      }
+      groups {
+        id
+        name
+        tasks {
+          endDate
+          id
+          importance
+          name
+          planned
+          startDate
+        }
+      }
+    }
+  }
+`;
+
+const SEND_TASK = gql`
+  mutation Mutation($name: String!, $importance: importanceInput!) {
+    addTask(name: $name, importance: $importance) {
+      id
+      importance
+      name
+    }
+  }
+`;
 export default function Home() {
+  //data.me.tasks taski indywidualne
+  //data.me.groups grupy w ktorych jest user group.task - taski danej grupy
+  const { data, loading, error } = useQuery(GET_TASKS); //task.planned - na godziny
+  const [taskMutation] = useMutation(SEND_TASK); // taskMutation({variables:{name:name,importance:{importance:ENUM}}})
+
   return (
     <div>
       <Head>
