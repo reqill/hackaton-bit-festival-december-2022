482a15bda365edd86cc13ab8bb280a88460d4169
Adam Maciuga <80308649+Mac-Adam@users.noreply.github.com>
2022-12-03 05:41:56 +0100

    feat(integration): added functions to retrieve and send data to api (#14)

--- pages/dashboard/calendar/index.tsx
+++ pages/dashboard/calendar/index.tsx
@@ -1,7 +1,54 @@
 import { Center, Heading } from '@chakra-ui/react';
+import { gql, useMutation, useQuery } from '@apollo/client';
 import Head from 'next/head';
-
+const SEND_EVENT = gql`
+  mutation Mutation(
+    $name: String!
+    $importance: importanceInput!
+    $startDate: String!
+    $endDate: String!
+  ) {
+    addEvent(name: $name, importance: $importance, startDate: $startDate, endDate: $endDate) {
+      id
+      endDate
+      importance
+      name
+      planned
+      startDate
+    }
+  }
+`;
+const GET_TASKS = gql`
+  query Query {
+    me {
+      tasks {
+        endDate
+        id
+        importance
+        name
+        planned
+        startDate
+      }
+      groups {
+        id
+        name
+        tasks {
+          endDate
+          id
+          importance
+          name
+          planned
+          startDate
+        }
+      }
+    }
+  }
+`;
 export default function Home() {
+  //data.me.tasks taski indywidualne
+  //data.me.groups grupy w ktorych jest user group.task - taski danej grupy
+  const { data, loading, error } = useQuery(GET_TASKS); //task.planned - na godziny
+  const [eventMutation] = useMutation(SEND_EVENT); // eventMutation({variables:{to co w sendTask startDate,endDate}})
   return (
     <div>
       <Head>
--- pages/dashboard/groups/index.tsx
+++ pages/dashboard/groups/index.tsx
@@ -1,7 +1,77 @@
 import { Heading } from '@chakra-ui/react';
+import { gql, useMutation, useQuery } from '@apollo/client';
 import Head from 'next/head';
-
+const SEND_GROUP_TASK = gql`
+  mutation AddGroupTask($name: String!, $importance: importanceInput!, $groupId: String!) {
+    addGroupTask(name: $name, importance: $importance, groupId: $groupId) {
+      id
+      importance
+      name
+      planned
+    }
+  }
+`;
+const SEND_GROUP_EVENT = gql`
+  mutation Mutation(
+    $name: String!
+    $importance: importanceInput!
+    $startDate: String!
+    $endDate: String!
+    $groupId: String!
+  ) {
+    addGroupEvent(
+      name: $name
+      importance: $importance
+      startDate: $startDate
+      endDate: $endDate
+      groupId: $groupId
+    ) {
+      endDate
+      id
+      importance
+      name
+      planned
+      startDate
+    }
+  }
+`;
+const CREATE_GROUP = gql`
+  mutation Mutation($name: String!) {
+    createGroup(name: $name) {
+      name
+      id
+    }
+  }
+`;
+const ADD_TO_GROUP = gql`
+  mutation AddGroupTask($email: String!, $groupId: String!) {
+    addToGroup(email: $email, groupId: $groupId) {
+      name
+      id
+    }
+  }
+`;
+const GET_GROUPS = gql`
+  query Query {
+    me {
+      groups {
+        id
+        name
+      }
+    }
+  }
+`;
 export default function Home() {
+  const { data, loading, error } = useQuery(GET_GROUPS);
+
+  const [taskGroupMut] = useMutation(SEND_GROUP_TASK); //jak dla indywidualnych dylko musisz podać groupId
+
+  const [eventGroupMut] = useMutation(SEND_GROUP_EVENT); //jak dla indywidualnych dylko musisz podać groupId
+
+  const [groupCreateMut] = useMutation(CREATE_GROUP); //podajesz name i tyle
+
+  const [addToGroupMutation] = useMutation(ADD_TO_GROUP); // podajesz email i groupId
+
   return (
     <div>
       <Head>
